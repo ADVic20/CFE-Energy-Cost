0a5090d05e57aeea70f496eb2fadce9ef64b8705
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-27 15:21:53 -0600

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
 
+from datetime import datetime
+
+
 from homeassistant.components.sensor import (
     SensorEntity
 )
+
 
 from homeassistant.helpers.update_coordinator import (
     CoordinatorEntity
@@ -14,6 +18,8 @@ from .const import DOMAIN
 
 
 
+
+
 async def async_setup_entry(
     hass,
     entry,
@@ -21,51 +27,113 @@ async def async_setup_entry(
 ):
 
 
-    data = hass.data[DOMAIN][
+    coordinator = hass.data[DOMAIN][
         entry.entry_id
-    ]
+    ]["coordinator"]
 
 
-    coordinator = data[
-        "coordinator"
+
+    sensors = [
+
+        CFESensor(
+            coordinator,
+            "consumo",
+            "CFE Consumo Periodo",
+            "kWh"
+        ),
+
+
+        CFESensor(
+            coordinator,
+            "energy_cost",
+            "CFE Costo Energía",
+            "MXN"
+        ),
+
+
+        CFESensor(
+            coordinator,
+            "iva",
+            "CFE IVA",
+            "MXN"
+        ),
+
+
+        CFESensor(
+            coordinator,
+            "dap",
+            "CFE DAP",
+            "MXN"
+        ),
+
+
+        CFESensor(
+            coordinator,
+            "total",
+            "CFE Recibo Estimado",
+            "MXN"
+        ),
+
+
+
+        CFESensor(
+            coordinator,
+            "tariff",
+            "CFE Tarifa",
+            None
+        ),
+
+
+
+        CFESensor(
+            coordinator,
+            "previous_reading",
+            "CFE Lectura Anterior",
+            "kWh"
+        ),
+
+
+
+        CFESensor(
+            coordinator,
+            "current_reading",
+            "CFE Lectura Actual",
+            "kWh"
+        ),
+
+
+
+        CFESensor(
+            coordinator,
+            "period_start",
+            "CFE Inicio Periodo",
+            None
+        ),
+
+
+
+        CFESensor(
+            coordinator,
+            "period_end",
+            "CFE Fin Periodo",
+            None
+        ),
+
+
     ]
+
 
 
     async_add_entities(
-
-        [
-
-            CFEConsumptionSensor(
-                coordinator
-            ),
-
-            CFEEnergyCostSensor(
-                coordinator
-            ),
-
-            CFEIVASensor(
-                coordinator
-            ),
-
-            CFEDAPSensor(
-                coordinator
-            ),
-
-            CFETotalSensor(
-                coordinator
-            ),
-
-            CFETariffSensor(
-                coordinator
-            )
-
-        ]
-
+        sensors
     )
 
 
 
-class CFEBaseSensor(
+
+
+
+class CFESensor(
     CoordinatorEntity,
     SensorEntity
 ):
@@ -74,20 +142,24 @@ class CFEBaseSensor(
     def __init__(
         self,
         coordinator,
+        key,
         name,
-        key
+        unit
     ):
+
 
         super().__init__(
             coordinator
         )
 
 
-        self._attr_name = (
-            f"CFE {name}"
-        )
-
         self.key = key
+
+
+        self._attr_name = name
+
+
+        self._attr_native_unit_of_measurement = unit
 
 
         self._attr_unique_id = (
@@ -95,163 +167,64 @@ class CFEBaseSensor(
         )
 
 
+
+
+
     @property
     def native_value(
         self
     ):
 
-        return (
-            self.coordinator
-            .data
-            .get(
-                self.key
+
+        if not self.coordinator.data:
+
+            return None
+
+
+
+        return self.coordinator.data.get(
+            self.key
+        )
+
+
+
+
+    @property
+    def extra_state_attributes(
+        self
+    ):
+
+
+        if not self.coordinator.data:
+
+            return {}
+
+
+
+        return {
+
+
+            "period_start":
+            self.coordinator.data.get(
+                "period_start"
+            ),
+
+
+            "period_end":
+            self.coordinator.data.get(
+                "period_end"
+            ),
+
+
+            "previous_reading":
+            self.coordinator.data.get(
+                "previous_reading"
+            ),
+
+
+            "current_reading":
+            self.coordinator.data.get(
+                "current_reading"
             )
-        )
 
-
-
-class CFEConsumptionSensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "Consumo",
-            "energy"
-        )
-
-
-    @property
-    def native_unit_of_measurement(
-        self
-    ):
-
-        return "kWh"
-
-
-
-class CFEEnergyCostSensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "Costo Energía",
-            "energy_cost"
-        )
-
-
-    @property
-    def native_unit_of_measurement(
-        self
-    ):
-
-        return "MXN"
-
-
-
-class CFEIVASensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "IVA",
-            "iva"
-        )
-
-
-    @property
-    def native_unit_of_measurement(
-        self
-    ):
-
-        return "MXN"
-
-
-
-class CFEDAPSensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "DAP",
-            "dap"
-        )
-
-
-    @property
-    def native_unit_of_measurement(
-        self
-    ):
-
-        return "MXN"
-
-
-
-class CFETotalSensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "Recibo Estimado",
-            "total"
-        )
-
-
-    @property
-    def native_unit_of_measurement(
-        self
-    ):
-
-        return "MXN"
-
-
-
-class CFETariffSensor(
-    CFEBaseSensor
-):
-
-
-    def __init__(
-        self,
-        coordinator
-    ):
-
-        super().__init__(
-            coordinator,
-            "Tarifa",
-            "tariff"
-        )
+        }
