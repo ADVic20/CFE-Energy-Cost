032293217aaec9002cedb81ea601ad61e72b5b48
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-27 11:32:26 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,44 +1,63 @@
-"""CFE Energy Cost."""
-
-from __future__ import annotations
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 from .const import DOMAIN
 
-PLATFORMS = ["sensor"]
+
+PLATFORMS = [
+    "sensor"
+]
 
 
 async def async_setup(
     hass: HomeAssistant,
-    config: dict,
-) -> bool:
-    """Set up integration."""
+    config: dict
+):
+
     return True
+
 
 
 async def async_setup_entry(
     hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Set up config entry."""
+    entry: ConfigEntry
+):
+
+    hass.data.setdefault(
+        DOMAIN,
+        {}
+    )
+
+
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
-        PLATFORMS,
+        PLATFORMS
     )
+
 
     return True
 
 
+
 async def async_unload_entry(
     hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Unload config entry."""
+    entry: ConfigEntry
+):
 
-    return await hass.config_entries.async_unload_platforms(
+    unload_ok = await hass.config_entries.async_unload_platforms(
         entry,
-        PLATFORMS,
+        PLATFORMS
     )
+
+
+    if unload_ok:
+
+        hass.data[DOMAIN].pop(
+            entry.entry_id
+        )
+
+
+    return unload_ok
