629884044fafcb4f1ab0c758b022d6055451ab11
ADVic20 <151901862+ADVic20@users.noreply.github.com>
2026-07-27 15:16:07 -0600

    Update config_flow.py

--- config_flow.py
+++ config_flow.py
@@ -1,30 +1,34 @@
+from __future__ import annotations
+
+
 import voluptuous as vol
 
+
 from homeassistant import config_entries
-from homeassistant.helpers import selector
-from datetime import date
+
+from homeassistant.core import callback
+
+from homeassistant.helpers.selector import (
+    SelectSelector,
+    SelectSelectorConfig,
+    SelectOptionDict,
+    EntitySelector,
+    EntitySelectorConfig
+)
+
 
 from .const import (
     DOMAIN,
-
-    CONF_NAME,
+    CONF_ENERGY_SENSOR,
     CONF_TARIFF,
     CONF_REGION,
-
-    CONF_ENERGY_SENSOR,
-
-    CONF_START_DATE,
-    CONF_CYCLE,
-
-    CONF_IVA,
-    CONF_DAP,
-
-    CONF_DASHBOARD,
-    CONF_HISTORY,
-    CONF_EXCEL,
-    CONF_PDF,
-    CONF_NOTIFICATIONS
+    CONF_PERIOD_START,
+    CONF_PERIOD_END,
+    CONF_PREVIOUS_READING,
+    CONF_CURRENT_READING
 )
+
+
 
 
 class CFEEnergyCostConfigFlow(
@@ -32,7 +36,9 @@ class CFEEnergyCostConfigFlow(
     domain=DOMAIN
 ):
 
+
     VERSION = 1
+
 
 
     async def async_step_user(
@@ -40,230 +46,129 @@ class CFEEnergyCostConfigFlow(
         user_input=None
     ):
 
-        return await self.async_step_general()
+
+        if user_input is not None:
+
+
+            return self.async_create_entry(
+                title=user_input.get(
+                    "name",
+                    "CFE Principal"
+                ),
+
+                data=user_input
+            )
 
 
 
-    async def async_step_general(
-        self,
-        user_input=None
-    ):
-
-
-        if user_input:
-
-            self.data = user_input
-
-            return await self.async_step_meter()
-
-
-
-        schema = vol.Schema(
+        data_schema = vol.Schema(
             {
 
+
                 vol.Required(
-                    CONF_NAME,
+                    "name",
                     default="CFE Principal"
+                ): str,
+
+
+
+                vol.Required(
+                    CONF_ENERGY_SENSOR
                 ):
-                str,
+                EntitySelector(
+                    EntitySelectorConfig(
+                        domain="sensor"
+                    )
+                ),
+
+
+
+                vol.Required(
+                    CONF_REGION,
+                    default="norte"
+                ):
+                SelectSelector(
+                    SelectSelectorConfig(
+                        options=[
+                            SelectOptionDict(
+                                value="norte",
+                                label="Norte"
+                            ),
+
+                            SelectOptionDict(
+                                value="centro",
+                                label="Centro"
+                            ),
+
+                            SelectOptionDict(
+                                value="sur",
+                                label="Sur"
+                            )
+                        ]
+                    )
+                ),
+
 
 
                 vol.Required(
                     CONF_TARIFF,
                     default="1C"
                 ):
-                vol.In(
-                    [
-                        "1",
-                        "1A",
-                        "1B",
-                        "1C",
-                        "DAC"
-                    ]
+                SelectSelector(
+                    SelectSelectorConfig(
+                        options=[
+                            SelectOptionDict(
+                                value="1",
+                                label="Tarifa 1"
+                            ),
+
+                            SelectOptionDict(
+                                value="1C",
+                                label="Tarifa 1C"
+                            ),
+
+                            SelectOptionDict(
+                                value="DAC",
+                                label="DAC"
+                            )
+                        ]
+                    )
                 ),
 
 
+
+
                 vol.Required(
-                    CONF_REGION,
-                    default="Norte"
+                    CONF_PERIOD_START
                 ):
-                str
+                str,
+
+
+
+                vol.Required(
+                    CONF_PERIOD_END
+                ):
+                str,
+
+
+
+                vol.Required(
+                    CONF_PREVIOUS_READING
+                ):
+                vol.Coerce(float),
+
+
+
+                vol.Required(
+                    CONF_CURRENT_READING
+                ):
+                vol.Coerce(float),
 
             }
         )
 
 
         return self.async_show_form(
-            step_id="general",
-            data_schema=schema
-        )
-
-
-
-    async def async_step_meter(
-        self,
-        user_input=None
-    ):
-
-
-        if user_input:
-
-            self.data.update(
-                user_input
-            )
-
-            return await self.async_step_billing()
-
-
-
-        schema = vol.Schema(
-            {
-
-                vol.Required(
-                    CONF_ENERGY_SENSOR
-                ):
-                selector.EntitySelector(
-                    selector.EntitySelectorConfig(
-                        domain="sensor",
-                        device_class="energy"
-                     )
-                )
-
-            }
-        )
-
-
-        return self.async_show_form(
-            step_id="meter",
-            data_schema=schema
-        )
-
-
-
-    async def async_step_billing(
-        self,
-        user_input=None
-    ):
-
-
-        if user_input:
-
-            self.data.update(
-                user_input
-            )
-
-            return await self.async_step_options()
-
-
-
-        schema = vol.Schema(
-            {
-
-                vol.Required(
-                    CONF_START_DATE
-                ):
-                selector.DateSelector(),
-
-
-                vol.Required(
-                    CONF_CYCLE,
-                    default="bimonthly"
-                ):
-                vol.In(
-                    [
-                        "monthly",
-                        "bimonthly"
-                    ]
-                )
-
-            }
-        )
-
-
-        return self.async_show_form(
-            step_id="billing",
-            data_schema=schema
-        )
-
-
-
-    async def async_step_options(
-        self,
-        user_input=None
-    ):
-
-
-        if user_input:
-
-            self.data.update(
-                user_input
-            )
-
-
-            return self.async_create_entry(
-                title=self.data[CONF_NAME],
-                data=self.data
-            )
-
-
-
-        schema = vol.Schema(
-            {
-
-
-                vol.Optional(
-                    CONF_IVA,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_DAP,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_DASHBOARD,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_HISTORY,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_EXCEL,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_PDF,
-                    default=True
-                ):
-                bool,
-
-
-                vol.Optional(
-                    CONF_NOTIFICATIONS,
-                    default=True
-                ):
-                bool
-
-            }
-        )
-
-
-        return self.async_show_form(
-            step_id="options",
-            data_schema=schema
+            step_id="user",
+            data_schema=data_schema
         )
